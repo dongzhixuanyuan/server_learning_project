c47977d3c47adf93e5e8b8e63333457f8cd5a25d
liudong <liudong@rd.netease.com>
2020-11-08 15:11:37 +0800

    feat:初始化一个SpringBoot项目

---
+++ spring_start/src/test/kotlin/com/liudong/server/spring_start/SpringStartApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.liudong.server.spring_start
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringStartApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
