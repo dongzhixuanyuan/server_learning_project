eac7bbbe5c797e2c1363affecc0d42ea2c10ca1a
liudong <liudong@rd.netease.com>
2021-01-06 15:45:38 +0800

    添加thymeleaf依赖作为前端展示页面，并学习thymeleaf使用

--- anti_swindle/build.gradle.kts
+++ anti_swindle/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.3")
     implementation("com.google.code.gson:gson:2.8.6")
 //    implementation("org.springframework.boot:spring-boot-starter-log4j:1.3.8.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 
 }
 
---
+++ anti_swindle/src/main/kotlin/com/magina/antiswindle/backgroundmanager/ResEditControlelr.kt
@@ -0,0 +1,36 @@
+package com.magina.antiswindle.backgroundmanager
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import java.util.*
+
+
+/**
+ * @description thymeleaf测试
+ *
+ * @author magina (735106520@qq.com)
+ * @date 2021/1/5 4:22 下午
+ */
+
+@Controller
+@RequestMapping("/manager/res")
+class ResEditControlelr {
+
+    @RequestMapping("/all")
+    fun test(model: Model): String {
+        val student = Student("张三", 1)
+        val students: MutableList<Student> = ArrayList()
+        students.add(student)
+        students.add(Student("李四", 2))
+        students.add(Student("王五", 3))
+        students.add(Student("二麻子", 4))
+        students.add(Student("三棒子", 5))
+
+        model.addAttribute("students", students)
+        model.addAttribute("selectedstudent",student)
+        return "hello"
+    }
+}
+
+data class Student(val name: String, val id: Int)
